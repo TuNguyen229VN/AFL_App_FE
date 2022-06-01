ba196920336c0c5b31e85203408a86532e8f2a2e
tuananh <tuananh200055@gmail.com>
2022-06-01 23:43:35 +0700

    update comment

--- src/components/TeamDetailComponent/CommentTeamDetail.js
+++ src/components/TeamDetailComponent/CommentTeamDetail.js
@@ -14,6 +14,11 @@ function CommentTeamDetail() {
   const [limit, setLimit] = useState(5);
   const [content,setContent] = useState("");
   const [contentError, setContentError] = useState("");
+  const [contentEdit, setContentEdit] = useState("");
+  const [edit, setEdit] = useState(0);
+  const [deletePop,setDeletePop] = useState(false);
+  const [contentEditError,setContentEditError] = useState("");
+  const [deleteId, setDeleteId] = useState(0);
    const getComment = async () => {
      try {
       console.log("page index" , pageIndex);
@@ -66,6 +71,44 @@ let navigate = useNavigate();
       console.log(err);
     }
   }
+
+  const updateComment = async(id) => {
+    try {
+      if(contentEdit == null || contentEdit ==""){
+        setContentError("Vui lòng nhập bình luận...");
+        return;
+      }
+      const data ={
+        id: id,
+        content :contentEdit,
+        status:"",
+      } 
+      const response = await axios.put(`https://afootballleague.ddns.net/api/v1/Comment`,data,{
+        headers:{
+          "Content-Type": "application/json",
+        }
+      });
+      setEdit(0);
+      setContentEdit("");
+      setContentEditError("");
+      await getComment();
+    }catch (err) {
+      console.log(err);
+    }
+  }
+
+  const deleteComment = async (id)=>{
+    try{
+      const response = await axios.delete(`https://afootballleague.ddns.net/api/v1/Comment?id=${id}`);
+      setDeletePop(false);
+      setDeleteId(0);
+      await getComment();
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+
   const formatDate = (date) => {
     const myArr = date.split("T");
       const day = myArr[0].split("-").reverse();
@@ -79,6 +122,7 @@ let navigate = useNavigate();
   return (
     <>     
         <div className="teamdetail__content cmtTeam">
+        {deletePop&&<div className="deleteShadow"></div>}
           <h3 className="cmtTeam__title">Bình luận</h3>
           <div className="totalcmt">
             <p className="number">{comment&&comment.countList} bình luận</p>
@@ -97,10 +141,29 @@ let navigate = useNavigate();
           </form>
           <div className="another__cmt">
             {comment&&comment.comments.map(comment =>(<div className="another__content">
+            {deletePop&&<div className="deletePopup">
+            <h2>Bạn muốn xóa bình luận này ?</h2>
+            <div className="btnWrap">
+                <button className="btnLoadMore"onClick={e =>{setDeletePop(false); e.preventDefault()}}>Hủy</button>
+                <button className="btnLoadMore"onClick={e =>{deleteComment(deleteId); e.preventDefault()}}>Chấp nhận</button>
+                </div>
+          </div>}
               <img src={comment.user.avatar} alt="dev" />
               <div>
+              {user&&comment.userId==user.userVM.id?<div className="modifyIcon">
+                <img onClick={()=> {setEdit(comment.id);setContentEdit(comment.content);}}className="editIcon" src="/assets/icons/edit.png" alt="edit" />
+                <img onClick={()=> {setDeletePop(true); setDeleteId(comment.id)}}className="editIcon" src="/assets/icons/delete.png" alt="edit" />
+              </div>:""}
                 <p className="name">{comment.user.username}</p>
-                <p className="content">{comment.content}</p>
+                {edit!=comment.id&&<p className="content">{comment.content}</p>}
+                <form className={`editComment_form ${edit==comment.id&&'active'}`}>
+                 <textarea onChange={e =>{setContentEdit(e.target.value);}} className="content" placeholder="Viết bình luận" value={contentEdit} />
+                <p className="error">{contentEditError}</p>
+                <div className="btnWrap">
+                <button className="btnLoadMore"onClick={e =>{setEdit(0);  e.preventDefault()}}>Hủy</button>
+                <button className="btnLoadMore"onClick={e =>{updateComment(comment.id); e.preventDefault()}}>Hoàn tất</button>
+                </div>
+                  </form>
                 <p className="time">{formatDate(comment.dateCreate)} {formatTime(comment.dateCreate)}</p>
               </div>
             </div>))}
