33c10b4a6cdf5c9c336a4f52a19149ad3ca17eb1
tuananh <tuananh200055@gmail.com>
2022-06-07 19:44:06 +0700

    schedule trong day

--- src/components/PlayerDetailComponent/MyTournamentInPlayer.js
+++ src/components/PlayerDetailComponent/MyTournamentInPlayer.js
@@ -1,8 +1,163 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
+import { Link,useParams  } from "react-router-dom";
+import Loading from "../LoadingComponent/Loading";
+import styles from "./styles/style.module.css";
+import { getAPI } from "../../api";
 function MyTournamentInPlayer() {
+  const [loading, setLoading] = useState(false);
+  const [tournament, setTournament] = useState([]);
+  const [pageIndex, setPageIndex] = useState(1);
+  const [numPage,setNumPage] = useState(0);
+  const [limit, setLimit] = useState(10);
+  const { idPlayer } = useParams();
+  const getTournament = () => {
+    setLoading(true);
+    let numOfContent = limit;
+    const afterURL = `tournaments/FootballPlayerID?footballPlayerID=${idPlayer}&sort=DESC&pageIndex=1&limit=${numOfContent}`;
+    const response = getAPI(afterURL);
+    response
+      .then((res) => {
+        setNumPage(Math.ceil(res.data.tournaments.length/10));
+        setTournament(res.data.tournaments);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    getTournament();
+  }, []);
+  useEffect(() => {
+    getTournament();
+  }, [limit]);
+
+  const formatDate = (date) => {
+    const day = new Date(date);
+    return (
+      String(day.getDate()).padStart(2, "0") +
+      "/" +
+      String(day.getMonth() + 1).padStart(2, "0") +
+      "/" +
+      day.getFullYear()
+    );
+  };
+  const getType = (id) => {
+    if (1 === id) {
+      return "Loại trực tiếp";
+    }
+    if (2 === id) {
+      return "Đá vòng tròn";
+    }
+    if (3 === id) {
+      return "Đá chia bảng";
+    } else {
+      return "";
+    }
+  };
+
+  // Get Feild
+  const getFeild = (id) => {
+    if (1 === id) {
+      return "| Sân 5";
+    }
+    if (2 === id) {
+      return "| Sân 7";
+    }
+    if (3 === id) {
+      return "| Sân 11";
+    } else {
+      return "";
+    }
+  };
   return (
-    <div>MyTournamentInPlayer</div>
+    <div className={styles.my__tournament}>
+    <h2 className={styles.title}>Danh sách giải đấu của bạn</h2>
+    <div className={styles.list__tournament}>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className={styles.tournament}>
+          {tournament.length > 0
+            ? tournament.map((tour) => (
+                <Link
+                  to={`/tournamentDetail/${tour.id}/inforTournamentDetail`}
+                  className={styles.item__tournament}
+                >
+                  <div className={styles.tournament__img}>
+                    <img
+                      src={tour.tournamentAvatar}
+                      alt={tour.tournamentName}
+                    />
+                  </div>
+                  <div className={styles.tournamnet__text}>
+                    <h2>{tour.tournamentName}</h2>
+                    <div className={styles.man}>
+                    <i class="fa-solid fa-bullhorn"></i>
+                      <span className={styles.sub__title}>
+                        Chế độ:{" "}
+                      </span>
+                      <span className={styles.text_field}>
+                       {tour.mode==="PRIVATE"?"Riêng tư":"Công khai"}
+                      </span>
+                    </div>
+                    <div className={styles.man}>
+                      <i>
+                        <img src="/public/assets/icons/join.png" alt="join"/>
+                      </i>
+                      <span className={styles.sub__title}>
+                        Đội tham gia:{" "}
+                      </span>
+                      <span className={styles.text_field}>
+                        {" "}
+                        {tour.numberTeamInTournament} /{" "}
+                        {tour.footballTeamNumber} đội
+                      </span>
+                    </div>
+                    <div className={styles.man}>
+                      <i className="fas fa-solid fa-font-awesome"></i>
+                      <span className={styles.sub__title}>Hình thức: </span>
+                      <span className={styles.text_field}>
+                        {" "}
+                        {getType(tour.tournamentTypeId)}{" "}
+                        {getFeild(tour.footballFieldTypeId)}
+                      </span>
+                    </div>
+                    <div className={styles.man}>
+                      <i className="fas fa-solid fa-location-dot"></i>
+                      <span className={styles.sub__title}>Địa điểm: </span>
+                      <span className={`${styles.text_field} ${styles.address}`}>
+                        {tour.footballFieldAddress}
+                      </span>
+                    </div>
+                    <div className={styles.man}>
+                      <i className="fa-solid fa-calendar-days"></i>
+                      <span className={styles.sub__title}>
+                        Thời gian diễn ra:{" "}
+                      </span>
+                      <span className={styles.text_field}>
+                        {formatDate(tour.tournamentStartDate)} -{" "}
+                        {formatDate(tour.tournamentEndDate)}{" "}
+                      </span>
+                    </div>
+                    <div className={styles.status}>
+                    {tour.status === true ? "Đang diễn ra" : "Đã kết thúc"}
+                  </div>
+                  </div>
+                  </Link>
+              
+              )): null}
+          {pageIndex<numPage?<button className="btnLoadMore" onClick={e =>{setPageIndex(pageIndex => pageIndex+1);
+              setLimit(limit => limit+10);  e.preventDefault();}}>Xem Thêm</button>:""}
+        </div>
+      )}
+      {tournament.length <= 0 ? (
+        <h1 className={styles.titleNoContent}>
+          Bạn chưa tham gia giải đấu nào
+        </h1>
+      ) : null}
+    </div>
+  </div>
   )
 }
 
--- src/components/PlayerDetailComponent/ScheduleInPlayer.js
+++ src/components/PlayerDetailComponent/ScheduleInPlayer.js
@@ -1,9 +1,79 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
+import { Link,useParams  } from "react-router-dom";
+import Loading from "../LoadingComponent/Loading";
+import { getAPI } from "../../api";
 function ScheduleInPlayer() {
+  const [loading, setLoading] = useState(false);
+  const { idPlayer } = useParams();
+  const [match, setMatch] = useState(); 
+  useEffect(()=>{
+    getAllMatch();
+  },[]);
+
+  const getAllMatch = () => {
+    setLoading(true);
+    const afterURL = `matchs/TournamentID?footballPlayerID=${idPlayer}`;
+    const response = getAPI(afterURL);
+    response
+      .then((res) => {
+        setMatch(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const formatDate = (date) => {
+    const myArr = date.split("T");
+      const day = myArr[0].split("-").reverse();
+       return day.join("/");
+  }
+  const formatTime = (date) => {
+    const myArr = date.split("T");
+      const day = myArr[1].split(".");
+      const time = day[0].split(":");
+       return time[0]+"h"+time[1];
+  }
   return (
-    <div>ScheduleInPlayer</div>
-  )
+    <>
+      <div className="teamdetail__content schedule__tour prediction">
+        <div className="wrap__table">
+          <table className="schedule__table">
+            <tr>
+              <th colSpan={5}>Các trận đấu hôm nay</th>
+            </tr>
+            {match&&match.matchs.map(match =>(<tr>
+              <td>{formatDate(match.matchDate)} {formatTime(match.matchDate)}</td>
+              <td>
+                {match.teamInMatches[0].teamName}
+                <img
+                  src={match.teamInMatches[0].team.teamAvatar}
+                  alt="gallery_item"
+                />
+              </td>
+              <td>
+                <span className="score">{match.teamInMatches[0].teamScore}</span>
+                <span className="score"> - </span>
+                <span className="score">{match.teamInMatches[1].teamScore}</span>
+              </td>
+              <td>
+                <img
+                  src={match.teamInMatches[1].team.teamAvatar}
+                  alt="gallery_item"
+                />
+               {match.teamInMatches[1].teamName}{" "}
+              </td>
+              <td>
+              {" "}
+              <Link  to={`/tournamentDetail/${match.tournamentId}/inforTournamentDetail`} style={{width:"130px"}}>Đến giải đấu</Link>
+            </td>
+            </tr>))}
+          </table>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default ScheduleInPlayer
