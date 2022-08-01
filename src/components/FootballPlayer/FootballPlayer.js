40c534600412865214ea94dca93ec96d5ee4e644
khoatase140107 <truonganhkhoa1405@gmail.com>
2022-08-01 20:40:37 +0700

    khoa commit

--- src/components/FootballPlayer/FootballPlayer.js
+++ src/components/FootballPlayer/FootballPlayer.js
@@ -23,11 +23,11 @@ export default function FootballPlayer() {
   const [listFootballPlayer, setListFootballPlayer] = useState(null);
   useEffect(() => {
     getAllFootballPlayer();
-  }, [nameFootballSearch, genderSearch, positionSearch, currentPage]);
+  }, [genderSearch, positionSearch, currentPage]);
   const getAllFootballPlayer = () => {
-    console.log(nameFootballSearch);
     setLoading(true);
     const dataSearch = `football-player-name=${nameFootballSearch}&gender=${genderSearch}&position=${positionSearch}`;
+    console.log(dataSearch);
     const response = getAllFootballPlayerAPI(dataSearch, currentPage);
     response
       .then((res) => {
@@ -35,7 +35,6 @@ export default function FootballPlayer() {
           setLoading(false);
           setCount(res.data.countList);
           setListFootballPlayer(res.data.footballPlayers);
-          console.log(res.data.footballPlayers);
         }
       })
       .catch((err) => {
@@ -58,19 +57,30 @@ export default function FootballPlayer() {
         break;
       case "gender":
         setGenderSearch(value);
+
         break;
       default:
         setPositionSearch(value);
+
         break;
     }
   };
 
   const onSubmitHandler = (e) => {
     e.preventDefault();
+    clickSearch();
   };
 
   const handlePageClick = (data) => {
     setCurrentPage(data.selected + 1);
+  };
+
+  const clickSearch = () => {
+    if (currentPage !== 1) {
+      setCurrentPage(1);
+    } else {
+      getAllFootballPlayer();
+    }
   };
   return (
     <div>
